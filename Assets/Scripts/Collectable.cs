8101c74b9f7644415e4bac2148c3b2b08144af2e
scarffy <34879344+scarffy@users.noreply.github.com>
2021-07-15 00:29:45 +0800

    Collectable Manager

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:499ef578d66d6e073c6e0449ec29b3aa47e1f82e0b8114fc6fce45c0f7d75db9
+size 287
---
+++ Assets/Scripts/CollectableManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:105c03e28b727cb5d849b98ce76beb3a68bf1b4150c4f2a3f56c94851e615530
+size 575
