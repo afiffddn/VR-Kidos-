86f6151a524e9c34ef0a3dac686b616adfd290b3
scarffy <34879344+scarffy@users.noreply.github.com>
2021-07-22 13:59:31 +0800

    Update MoveSceneLevel1.cs
    Disable editing in inspector

--- Assets/Scripts/MoveSceneLevel1.cs
+++ Assets/Scripts/MoveSceneLevel1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ba40d4aed44942d196d6c4ec3102c3459e5781f20e92630f104e12db9b16994
-size 1607
+oid sha256:52404ef3281ce50317eff5477d57543428167d1e7a8f1ff8c77ae7efe7f08c9f
+size 1590
