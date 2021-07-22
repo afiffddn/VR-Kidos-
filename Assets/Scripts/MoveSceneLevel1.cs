a0e914291decfa37318be1bb1b2753005e322bf6
scarffy <34879344+scarffy@users.noreply.github.com>
2021-07-22 13:57:12 +0800

    Update MoveSceneLevel1.cs
    Update MoveScene to adapt by level

--- Assets/Scripts/MoveSceneLevel1.cs
+++ Assets/Scripts/MoveSceneLevel1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb02147cfc100a85a783c8237ca79683bd0ac5fa6d5c3116bce3af801abc3292
-size 993
+oid sha256:3ba40d4aed44942d196d6c4ec3102c3459e5781f20e92630f104e12db9b16994
+size 1607
