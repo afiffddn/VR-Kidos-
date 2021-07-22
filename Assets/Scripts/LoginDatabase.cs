3482d3e92e25cf4bba413d9085468308e18252fc
scarffy <34879344+scarffy@users.noreply.github.com>
2021-07-22 10:09:55 +0800

    Update LoginDatabase.cs
    Fix teacher and student login

--- Assets/Scripts/LoginDatabase.cs
+++ Assets/Scripts/LoginDatabase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8bba347e4a841095ebb1d6349ad0fdc5b4e3181a3c9d1a569c760494caa6f3d
-size 1763
+oid sha256:065e5497923c0120f3cb2e74fa976e568b3a7057ebb13932707cde3768c8aef2
+size 1946
